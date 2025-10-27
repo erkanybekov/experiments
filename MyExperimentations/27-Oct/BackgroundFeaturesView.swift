519d81146c0f98e1617a11f09e5735655da6dea0
Erlanbek <kanybekov668@gmail.com>
2025-10-27 18:47:14 +0600

    hint to the future learning about push notifications: Live Activity, bgScheduleTask

---
+++ MyExperimentations/27-Oct/BackgroundFeaturesView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundFeaturesView.swift
+//  MyExperimentations
+//
+//  Created by Erlan Kanybekov on 10/27/25.
+//
+
+import SwiftUI
+
+struct BackgroundFeaturesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundFeaturesView()
+}
